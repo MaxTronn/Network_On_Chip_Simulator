c4b961d55ca2b8ee091a2ad28427c82d15f4d723
ash26n <ashwin20185@iiitd.ac.in>
2022-11-07 23:10:59 +0530

    removing redundant parameter

--- port.py
+++ port.py
@@ -6,7 +6,7 @@ class port:
     def send_to_port(self, incoming_flit):
         self.port_buffer.put(incoming_flit)
     
-    def send_from_port(self, outgoing_flit):
+    def send_from_port(self):
         return self.port_buffer.get
 
         
