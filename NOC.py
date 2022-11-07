16a816255a7d296fdb3cc7080dfb00ea36ba0861
ash26n <88731109+ash26n@users.noreply.github.com>
2022-11-07 21:35:46 +0530

    mesh_connect router_list redefine

--- NOC.py
+++ NOC.py
@@ -2,7 +2,7 @@ import Router
 
 class NOC :
 
-    router_list = []  # Routers [A,B,C,D]
+    router_list = [[],[]]  # Routers [[A,B],[D,C]]
     packet_list = []
 
     def __init__(self, traffic_file, routing_algo, latency, cycle_list, packet_list) :
@@ -13,28 +13,32 @@ class NOC :
         self.packet_list = packet_list
 
     # This function initilialises the router objects and defines connections between them.
-    def initialize_router_list(self):
-        for i in range(4) :
-            self.router_list.append(Router())
+    # def initialize_router_list(self):
+    #     for i in range(4) :
+    #         self.router_list.append(Router())
+def initialize_router_list(self):
+         for i in range(4) :
+             self.router_list.append(Router())
 
         # Defining connections
-
+def mesh_connect(self):
         # A -- B Connection
-        self.router_list[0].east = self.router_list[1]
-        self.router_list[1].west = self.router_list[0]
+        self.router_list[0][0].east = self.router_list[0][1].west
+        self.router_list[0][1].west = self.router_list[0][0].east
 
         # B -- C Connection
-        self.router_list[1].south = self.router_list[2]
-        self.router_list[2].north = self.router_list[1]
+        self.router_list[0][1].south = self.router_list[1][1].north
+        self.router_list[1][1].north = self.router_list[0][1].south
 
         # C -- D Connection
-        self.router_list[2].east = self.router_list[3]
-        self.router_list[3].west = self.router_list[2]
+        self.router_list[1][0].east = self.router_list[1][1].west
+        self.router_list[1][1].west = self.router_list[1][0].east
 
         # A -- D Connection
-        self.router_list[0].south = self.router_list[3]
-        self.router_list[3].north = self.router_list[0]
+        self.router_list[0][0].south = self.router_list[1][0].north
+        self.router_list[1][0].north = self.router_list[0][0].south
 
 
     # This function handles the communication part of the NOC
     def start_communication(self):
+        
