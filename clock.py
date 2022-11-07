4583eb1fbda7726954cd3abe83c4f003119d4177
AkshatIIITD <akshat20172@iiitd.ac.in>
2022-11-07 22:49:04 +0530

    crossbar added and clock removed

--- clock.py
+++
@@ -1,7 +0,0 @@
-import time
-
-
-class Clock:
-
-    def __init__(self):
-        self.cycleNo = 0
---
+++ crossbar.py
@@ -0,0 +1,15 @@
+class Xbar:
+
+    def __init__(self):
+        self.input_port = None
+        self.output_port = None
+
+    def connect(self, input_port, output_port):
+        self.input_port = input_port
+        self.output_port = output_port
+        self.transfer_data()
+
+    def transfer_data(self):
+        self.output_port.buffer.put(self.input_port.buffer.get())
+
+
