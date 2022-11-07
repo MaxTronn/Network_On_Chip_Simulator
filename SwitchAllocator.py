50ed7134b6d238c2957ee00897ff073f1347a8d3
Ullas2021 <88886793+Ullas2021@users.noreply.github.com>
2022-11-08 04:06:54 +0530

    Add files via upload

---
+++ SwitchAllocator.py
@@ -0,0 +1,45 @@
+# switch allocator needs to be called every cycle where flit transfer through a particular router needs to takes place.
+# At each decisive cycle the reallocate() function reallocates the input and output ports of the XBar and returns the
+# ports that need to be enabled by the XBar for that cycle
+router_list  =['A', 'B', 'C', 'D']
+class SwitchAllocator:
+
+    def __init__(self, ports):
+        self.routing = 'XY'
+        self.src = None
+        self.dest = None
+
+    def reallocate(self, source, destination):
+
+        self.src = source
+        self.dest = destination
+
+        if(self.src == '00' and self.dest == '10'):
+
+            router_list[1].west = router_list[0].east
+            router_list[1].south = router_list[1].west
+            router_list[2].north = router_list[1].south
+
+        if(self.src == '01' and self.dest == '11'):
+
+            router_list[0].east = router_list[1].west
+            router_list[0].south = router_list[0].east
+            router_list[3].north = router_list[0].south
+
+        if(self.src == '10' and self.dest == '00'):
+
+            router_list[3].east = router_list[2].west
+            router_list[3].north = router_list[3].east
+            router_list[0].south = router_list[3].north
+
+        if(self.src == '11' and self.dest == '01'):
+
+            router_list[2].west = router_list[3].east
+            router_list[2].north = router_list[2].west
+            router_list[1].south = router_list[2].north
+
+
+
+
+
+
