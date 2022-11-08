3890112d302f3ca3242bb61c22da6f027c9c5235
Ullas2021 <ullas20255@iiitd.ac.in>
2022-11-08 12:35:49 +0530

    main

--- main.py
+++ main.py
@@ -15,15 +15,19 @@ if __name__ == '__main__':
             src = bin(int(bitword[1])).replace("0b", "")
             dest = bin(int(bitword[2])).replace("0b", "")
             loadbits = bitword[3]
-            header = bin(random.getrandbits(28)).replace("0b", "") + dest + src + "00"
+            header = "00" + src + dest + bin(random.getrandbits(28)).replace("0b", "")
             payload = []
             for j in range(3):
-                payload.append(loadbits[j * 32:(j + 1) * 32] + "01")
-            tail = bin(random.getrandbits(32)).replace("0b", "") + "11"
+                payload.append("01" + loadbits[j * 32:(j + 1) * 32])
+            tail = "11" + bin(random.getrandbits(32)).replace("0b", "")
             cycleList.append(cycle)
             packet = [header, payload[0], payload[1], payload[2], tail]
             packetList.append(packet)
 
             print(header + "\n" + payload[0] + "\n" + payload[1] + "\n" + payload[2] + "\n" + tail)
+
+
+
+
 
 
